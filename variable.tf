771b9da61000dbaeb764533b0e1d19e7de1bca77
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2025-02-16 19:03:18 -0300

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,94 @@
+variable "rg_name" {
+    description = "Nome Resource Group (Marcelo)"
+    type = string
+}
+
+variable "rg_location" {
+    description = "Localização RG"
+    type = string
+}
+
+variable "service_plan_name" {
+    description = "NOME SERVICE PLAN"
+    type = string
+}
+
+variable "service_plan_name2" {
+    description = "NOME SERVICE PLAN2"
+    type = string
+}
+
+variable "service_plan_location1" {
+    description = "Localização SERVICE PLAN"
+    type = string
+}
+
+variable "service_plan_location2" {
+    description = "Localização SERVICE PLAN"
+    type = string
+}
+
+variable "win_web_app" {
+    description = "Localização win web"
+    type = string
+}
+
+variable "win_web_app2" {
+    description = "Localização win web2"
+    type = string
+}
+
+variable "frontdoor_name" {
+    description = "Nome FrontDoor Profile"
+    type = string
+}
+
+variable "frontdoor_sku_name" {
+    description = "SKU Frontdoor"
+    type = string
+}
+
+variable "frontdoor_endpoint_name" {
+    description = "Nome Endpoint Frontdoor"
+    type = string
+}
+
+variable "origin_group_name" {
+    description = "Nome Origin Group"
+    type = string
+}
+
+variable "origin_name" {
+    description = "Nome Origin Group"
+    type = string
+}
+
+variable "origin2_name" {
+    description = "Nome Origin Group"
+    type = string
+}
+
+variable "host_name_origin1" {
+    description = "Nome Host-name Origin1"
+    type = string
+}
+
+variable "host_reader1" {
+    description = "Nome Host-reader1 Origin1"
+    type = string
+}
+
+variable "host_name_origin2" {
+    description = "Nome Host-name Origin1"
+    type = string
+}
+
+variable "host_reader2" {
+    description = "Nome Host-reader1 Origin1"
+    type = string
+}
+
+variable "route_name" {
+    description = "Nome da Rota"
+    type = string
+}
